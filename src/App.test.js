ffc913c76370dc63f547d0377aab5182b120d512
davidFox <djfoxy11@gmail.com>
2023-06-05 12:01:15 -0600

    Add tests to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
+import BookingPage from './components/BookingPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the Reservations page correctly", () => {
+  render(
+    <BrowserRouter>
+      <BookingPage />
+    </BrowserRouter>
+  );
+  const submitButton = screen.getByText("Make your reservation");
+  expect(submitButton).toBeInTheDocument();
+});
+
+test("Guests updated", () => {
+  render(
+    <BrowserRouter>
+      <BookingPage />
+    </BrowserRouter>
+  );
+  const guestsTotal = screen.getByLabelText(/Number of guests/);
+  fireEvent.change(guestsTotal, { target: { value: "5" } });
+  expect(guestsTotal.value).toEqual("5");
+});
+
+test("Date updated", () => {
+  render(
+    <BrowserRouter>
+      <BookingPage />
+    </BrowserRouter>
+  );
+  const date = screen.getByLabelText(/Choose date/);
+  fireEvent.change(date, { target: { value: "2023-06-15" } });
+  expect(date.value).toEqual("2023-06-15");
+});
+
+test("Time updated", () => {
+  render(
+    <BrowserRouter>
+      <BookingPage />
+    </BrowserRouter>
+  );
+  const timeSelection = screen.getByLabelText(/Choose time/);
+  fireEvent.change(timeSelection, { target: { value: "19:00" } });
+  expect(timeSelection.value).toEqual("19:00");
 });
