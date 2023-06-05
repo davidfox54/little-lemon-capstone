b2272014eaf268cc5ff7840253b29d06c2f2e335
davidFox <djfoxy11@gmail.com>
2023-06-05 12:09:35 -0600

    Update some broken styling after removing the card tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,22 +17,22 @@ function Footer() {
                     <h5>Doormat navigation</h5>
                     <NavLinks />
                 </nav>
-                <article className='footer-contact'>
+                <div className='footer-contact'>
                     <h5>Contact</h5>
                     <ul>
                         <li><b>Address:</b> 123 Blvd ln</li>
                         <li><b>Email:</b> noreply@gmail.com</li>
                         <li><b>Phone Number:</b> 111-222-3333</li>
                     </ul>
-                </article>
-                <article className='footer-socials'>
+                </div>
+                <div className='footer-socials'>
                     <h5>Social Media Links</h5>
                     <ul>
                         <li>Instagram</li>
                         <li>Twitter</li>
                         <li>Facebook</li>
                     </ul>
-                </article>
+                </div>
             </div>
         </footer>
     )
